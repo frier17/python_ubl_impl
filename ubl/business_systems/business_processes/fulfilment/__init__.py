86821dae6b4b95f0b179c629fd46939f05f22da6
Code MONK <aniefiok.friday@gmail.com>
2019-01-04 09:53:48 +0100

    created and modified the business_management package. This package will provide tools for auxiliary services such as publishing notice of tender to supplier's profile etc.

--- ubl/business_systems/business_processes/fulfilment/__init__.py
+++ ubl/business_systems/business_processes/fulfilment/__init__.py
@@ -1,7 +1,78 @@
 from abc import abstractmethod
 
 
-class DespatchAdviceMixin:
+class FulfilmentMixin:
+    __slots__ = ()
+
+
+class FulfilmentDescriptor:
+    __slots__ = 'fulfilment'
+
+    def __get__(self, instance, owner):
+        pass
+
+    def __set__(self, instance, value):
+        pass
+
+    def __delete__(self, instance):
+        pass
+
+
+class Fulfilment(FulfilmentMixin):
+    class Consignor(FulfilmentDescriptor):
+        pass
+
+    class Consignee(FulfilmentDescriptor):
+        pass
+
+    class Shipper(FulfilmentDescriptor):
+        pass
+
+    class Recipient(FulfilmentDescriptor):
+        pass
+
+    class Supplier(FulfilmentDescriptor):
+        pass
+
+    class Buyer(FulfilmentDescriptor):
+        pass
+
+    class Forwarder(FulfilmentDescriptor):
+        pass
+
+    class FreightAgents(FulfilmentDescriptor):
+        pass
+
+    class Consignments(FulfilmentDescriptor):
+        pass
+
+    consignors = Consignor()
+    consignees = Consignee()
+    shipper = Shipper()
+    recipient = Recipient()
+    supplier = Supplier()
+    buyer = Buyer()
+    freight_forwarder = Forwarder()
+    freight_agents = FreightAgents()
+    consignments = Consignments()
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class SimpleFulfilment(Fulfilment):
+    pass
+
+
+class ConsolidatedFulfilment(Fulfilment):
+    pass
+
+
+class IntermediaryFulfilment(Fulfilment):
+    pass
+
+
+class DispatchAdviceMixin:
     # FULFILLMENT WITH DESPATCH ADVICE PROCESS
     __slots__ = ()
 
@@ -22,7 +93,7 @@ class DespatchAdviceMixin:
         pass
 
     @abstractmethod
-    def cancel_despatch(self, *args, **kwargs):
+    def cancel_dispatch(self, *args, **kwargs):
         pass
 
     @abstractmethod
@@ -38,12 +109,13 @@ class DespatchAdviceMixin:
         pass
 
     @abstractmethod
-    def send_despatch_advice(self, *args, **kwargs):
+    def send_dispatch_advice(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def receive_despatch_advice(self, *args, **kwargs):
+    def receive_dispatch_advice(self, *args, **kwargs):
         pass
+
 
 class ReceiptAdviceMixin:
     # FULFILLMENT WITH RECEIPT ADVICE PROCESS
