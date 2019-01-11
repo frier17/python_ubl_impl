b9ef851f86b71dcd0127be3e98a6bccc21a86b1c
Code MONK <aniefiok.friday@gmail.com>
2019-01-11 22:06:24 +0100

    Modified the billing mixin merging Utility, Payment, and other billing into single package

--- ubl/business_processes/billing/__init__.py
+++ ubl/business_processes/billing/__init__.py
@@ -2,11 +2,20 @@ from abc import abstractmethod
 from ubl.business_processes import BusinessService
 
 
+@abstractmethod
 class BillingMixin:
     __slots__ = ()
 
     @abstractmethod
+    def receive_invoice(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
     def send_account_response(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def receive_credit_note(self, *args, **kwargs):
         pass
 
     @abstractmethod
@@ -14,37 +23,23 @@ class BillingMixin:
         pass
 
     @abstractmethod
-    def receive_invoice(self, *args, **kwargs):
+    def raise_credit_note(self, *args, **kwargs):
         pass
 
     @abstractmethod
     def raise_invoice(self, *args, **kwargs):
         pass
 
-
-class CreditNoteBillingMixin:
-    """
-    The credit note billing is a subset of the billing service in which a
-    buyer's account may be credited by a given sum to compensate for damage
-    goods, overly invoiced item, wrongly applied discount etc. This service
-    mixin provides the necessary business activities for billing using a
-    credit note.
-
-    See more information at:
-    http://docs.oasis-open.org/ubl/os-UBL-2.1/UBL-2.1.html#S-BILLING
-    """
-    __slots__ = ()
-
     @abstractmethod
-    def receive_credit_note(self, *args, **kwargs):
+    def raise_self_billed_invoice(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def raise_credit_note(self, *args, **kwargs):
+    def receive_self_billed_invoice(self, *args, **kwargs):
         pass
 
 
-class DebitNoteBillingMixin:
+class BillingWithDebitNoteMixin:
     """
     The debit note billing is a subset of the billing service in which a
     buyer is informed of amount due a seller for ordered item, despatch
@@ -71,20 +66,16 @@ class SelfCreditNoteBillingMixin:
     traditional billing services. In this scenario, the customer raises an
     invoice on behalf of a supplier. Both supplier and customer are
     responsible for providing taxation information
+
+    The credit note billing is a subset of the billing service in which a
+    buyer's account may be credited by a given sum to compensate for damage
+    goods, overly invoiced item, wrongly applied discount etc. This service
+    mixin provides the necessary business activities for billing using a
+    credit note.
+
+    See more information at:
+    http://docs.oasis-open.org/ubl/os-UBL-2.1/UBL-2.1.html#S-BILLING
     """
-    __slots__ = ()
-
-    @abstractmethod
-    def raise_self_billed_invoice(self, *args, **kwargs):
-        pass
-
-    @abstractmethod
-    def receive_self_billed_invoice(self, *args, **kwargs):
-        pass
-
-
-class SelfDebitNoteBillingMixin:
-    # SELF BILLING WITH SELF BILLED CREDIT NOTE PROCESS
     __slots__ = ()
 
     @abstractmethod
@@ -98,7 +89,8 @@ class SelfDebitNoteBillingMixin:
 
 class PaymentReminderMixin:
     """
-    A payment reminder is service notifies the customer of accounts due to be
+    A payment reminder is service notifies the customer of accounts due
+    to be
     paid.
     """
     __slots__ = ()
@@ -112,9 +104,66 @@ class PaymentReminderMixin:
         pass
 
 
-class BillingService(BusinessService, BillingMixin, CreditNoteBillingMixin,
-                     DebitNoteBillingMixin, PaymentReminderMixin,
-                     SelfCreditNoteBillingMixin, SelfDebitNoteBillingMixin):
+class FreightBillingMixin:
+    __slots__ = ()
+
+    @abstractmethod
+    def receive_freight_invoice(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def send_freight_invoice_to_cosignor(self, *args, **kwargs):
+        pass
+
+
+class UtilityBillingMixin:
+    __slots__ = ()
+
+    @abstractmethod
+    def receive_utility_statement(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def report_usage(self, *args, **kwargs):
+        pass
+
+
+class PaymentNotificationMixin:
+    __slots__ = ()
+
+    @abstractmethod
+    def authorize_payment(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def notify_of_payment(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def receive_advice(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def notify_payee(self, *args, **kwargs):
+        pass
+
+
+class PaymentStatementMixin:
+    __slots__ = ()
+
+    @abstractmethod
+    def receive_statement(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def guarantee_statement(self, *args, **kwargs):
+        pass
+
+
+class BillingService(BusinessService, BillingMixin,BillingWithDebitNoteMixin,
+                     FreightBillingMixin, PaymentNotificationMixin,
+                     PaymentReminderMixin, PaymentStatementMixin,
+                     SelfCreditNoteBillingMixin, UtilityBillingMixin):
     """
     In the Billing process, a request is made for payment for goods or
     services that have been ordered, received, or consumed. In practice,
@@ -122,7 +171,6 @@ class BillingService(BusinessService, BillingMixin, CreditNoteBillingMixin,
     Document types in these processes are Invoice, Credit Note, Debit Note,
     and Application Response.
     """
-
     __slots__ = 'invoice', 'credit_note', 'application_response', \
                 'debit_note', 'self_billed_invoice', 'payment_reminder'
 
@@ -135,26 +183,14 @@ class BillingService(BusinessService, BillingMixin, CreditNoteBillingMixin,
         self.payment_reminder = None
 
     @classmethod
-    def initialize(cls):
+    def initialize(cls, *args, **kwargs):
         pass
 
-    def receive_invoice(self, *args, **kwargs):
-        super().receive_invoice(*args, **kwargs)
+    def receive_statement(self, *args, **kwargs):
+        super().receive_statement(*args, **kwargs)
 
-    def send_account_response(self, *args, **kwargs):
-        super().send_account_response(*args, **kwargs)
-
-    def receive_account_response(self, *args, **kwargs):
-        super().receive_account_response(*args, **kwargs)
-
-    def raise_invoice(self, *args, **kwargs):
-        super().raise_invoice(*args, **kwargs)
-
-    def receive_credit_note(self, *args, **kwargs):
-        super().receive_credit_note(*args, **kwargs)
-
-    def raise_credit_note(self, *args, **kwargs):
-        super().raise_credit_note(*args, **kwargs)
+    def guarantee_statement(self, *args, **kwargs):
+        super().guarantee_statement(*args, **kwargs)
 
     def receive_debit_note(self, *args, **kwargs):
         super().receive_debit_note(*args, **kwargs)
@@ -162,11 +198,47 @@ class BillingService(BusinessService, BillingMixin, CreditNoteBillingMixin,
     def raise_debit_note(self, *args, **kwargs):
         super().raise_debit_note(*args, **kwargs)
 
-    def receive_self_billed_invoice(self, *args, **kwargs):
-        super().receive_self_billed_invoice(*args, **kwargs)
+    def raise_self_billed_credit_note(self, *args, **kwargs):
+        super().raise_self_billed_credit_note(*args, **kwargs)
+
+    def receive_self_billed_credit_note(self, *args, **kwargs):
+        super().receive_self_billed_credit_note(*args, **kwargs)
+
+    def receive_freight_invoice(self, *args, **kwargs):
+        super().receive_freight_invoice(*args, **kwargs)
+
+    def send_freight_invoice_to_cosignor(self, *args, **kwargs):
+        super().send_freight_invoice_to_cosignor(*args, **kwargs)
+
+    def raise_credit_note(self, *args, **kwargs):
+        super().raise_credit_note(*args, **kwargs)
 
     def raise_self_billed_invoice(self, *args, **kwargs):
         super().raise_self_billed_invoice(*args, **kwargs)
+
+    def receive_account_response(self, *args, **kwargs):
+        super().receive_account_response(*args, **kwargs)
+
+    def receive_credit_note(self, *args, **kwargs):
+        super().receive_credit_note(*args, **kwargs)
+
+    def receive_self_billed_invoice(self, *args, **kwargs):
+        super().receive_self_billed_invoice(*args, **kwargs)
+
+    def send_account_response(self, *args, **kwargs):
+        super().send_account_response(*args, **kwargs)
+
+    def receive_invoice(self, *args, **kwargs):
+        super().receive_invoice(*args, **kwargs)
+
+    def raise_invoice(self, *args, **kwargs):
+        super().raise_invoice(*args, **kwargs)
+
+    def receive_utility_statement(self, *args, **kwargs):
+        super().receive_utility_statement(*args, **kwargs)
+
+    def report_usage(self, *args, **kwargs):
+        super().report_usage(*args, **kwargs)
 
     def request_payment_of_account(self, *args, **kwargs):
         super().request_payment_of_account(*args, **kwargs)
@@ -174,8 +246,15 @@ class BillingService(BusinessService, BillingMixin, CreditNoteBillingMixin,
     def receive_reminder(self, *args, **kwargs):
         super().receive_reminder(*args, **kwargs)
 
-    def receive_self_billed_credit_note(self, *args, **kwargs):
-        super().receive_self_billed_credit_note(*args, **kwargs)
+    def notify_of_payment(self, *args, **kwargs):
+        super().notify_of_payment(*args, **kwargs)
 
-    def raise_self_billed_credit_note(self, *args, **kwargs):
-        super().raise_self_billed_credit_note(*args, **kwargs)
+    def authorize_payment(self, *args, **kwargs):
+        super().authorize_payment(*args, **kwargs)
+
+    def receive_advice(self, *args, **kwargs):
+        super().receive_advice(*args, **kwargs)
+
+    def notify_payee(self, *args, **kwargs):
+        super().notify_payee(*args, **kwargs)
+
